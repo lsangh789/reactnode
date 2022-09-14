2e15a1abec52591ba88a9c6836f9e505185071c5
lsangh789 <lsangh789@google.com>
2022-09-14 14:53:00 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="gray-background">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello react
-        </p>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/any',
+    'name': '홍길동',
+    'birthday': '961212',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961212',
+    'gender': '남자2',
+    'job': '대학생2'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '961211',
+    'gender': '남자3',
+    'job': '대학생3'
+  },
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
